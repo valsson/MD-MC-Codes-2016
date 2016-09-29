e283640aa680bf4b52981fc08dfc31e249246ab3
Omar Valsson <omar.valsson@gmail.com>
2016-09-29 16:29:02 +0200

    Changes and addtions to LJ MC

--- LJ-MC-sampling/MC_LennardJones.py
+++ LJ-MC-sampling/MC_LennardJones.py
@@ -1,21 +1,42 @@
 import numpy as np
+import copy
 
-from LennardJonesModel import LennardJonesModel
-from ParticleContainer import ParticleContainer
+from ParticleTools import *
 
+def getLJpotential(r2):
+    if r2 <= r2_cutoff:
+        inv_r2 = sigma**2/r2
+        value = 4.0*epsilon*(inv_r2**6-inv_r2**3)-ener_cutoff
+    else:
+        value = 0.0
+    return value
+#-------------------------------
 
+def getEnergyFromDistances(squared_distances):
+    energy = 0.0;
+    for i in xrange(squared_distances.shape[0]):
+        if squared_distances[i]<r2_cutoff: energy += getLJpotential(squared_distances[i])
+    return energy
+#-------------------------------
 
 # Parameters:
+sigma = 1.0
+epsilon = 1.0
 InitialSeed = -1
 kBoltzman = 1.0
 temperature = 0.7
+particle_type = "Ar"
 beta = 1.0/(kBoltzman*temperature)
 r_cutoff = 2.5
-num_particles = 64
-BoxLength = 5.1
+r2_cutoff = r_cutoff**2
+r2_cutoff = float("inf")
+ener_cutoff = 0.0
+ener_cutoff = getLJpotential(r2_cutoff)
+num_particles = 128
+BoxLength = 6.1
 cell = np.array([BoxLength, BoxLength, BoxLength])
-num_mc_sweeps = 100
-max_displacement = 1.0
+num_mc_sweeps = 1000
+max_displacement = 0.2
 nlist_update = 5
 #
 fn_traj = "traj.xyz"
@@ -24,11 +45,10 @@ fn_traj = "traj.xyz"
 r_nlist = r_cutoff + 2*nlist_update*max_displacement
 if InitialSeed >= 0: np.random.seed(InitialSeed)
 
-lj = LennardJonesModel(cell=cell,r_cutoff=r_cutoff)
-particles = ParticleContainer(num_particles=num_particles,cell=cell,particle_type="LJ",r_nlist=r_nlist)
-particles.randomizePostions()
-particles.writePostionsToFile(fn_traj,wrap_pbc=True,append=False)
 
+postions = np.zeros([num_particles,3])
+randomizePostions(postions,cell)
+writePostionsToFile(fn_traj,postions,[particle_type],cell,False)
 
 total_moves = 0
 accepted_moves = 0
@@ -37,21 +57,29 @@ for i in xrange(num_mc_sweeps):
     for k in xrange(num_particles):
         total_moves += 1
 
-        current_postions = particles.postions
-        current_energy = lj.getEnergyFromPostions(current_postions)
-
         rnd_index = np.random.randint(num_particles)
-        trial_postions = particles.newPostionsFromRandomDisplacement(max_displacement,particle_index=rnd_index)
-        trial_energy = lj.getEnergyFromPostions(trial_postions)
+        trial_postions = np.copy(postions)
+        randomDisplacement(trial_postions,max_displacement,rnd_index)
 
+        current_r2 =   getAllSquaredDistancesForOneParticle(postions,rnd_index,r2_cutoff,cell)
+        current_energy = getEnergyFromDistances(current_r2)
+        trial_r2 =     getAllSquaredDistancesForOneParticle(trial_postions,rnd_index,r2_cutoff,cell)
+        trial_energy =   getEnergyFromDistances(trial_r2)
         delta_energy = trial_energy - current_energy
+        #
+        # current_r2_B =   getAllSquaredDistances(postions,r2_cutoff,cell)
+        # current_energy_B = getEnergyFromDistances(current_r2_B)
+        # trial_r2_B =     getAllSquaredDistances(trial_postions,r2_cutoff,cell)
+        # trial_energy_B = getEnergyFromDistances(trial_r2_B)
+        # delta_energy_B = trial_energy_B - current_energy_B
+
         prob = np.exp(-beta*delta_energy)
         if prob>1.0: prob=1.0
         if prob > np.random.uniform(0.0,1.0):
             accepted_moves += 1
-            particles.postion = trial_postions
+            postions = np.copy(trial_postions)
     #--------------------------
-    particles.writePostionsToFile(fn_traj,wrap_pbc=True,append=True)
+    writePostionsToFile(fn_traj,postions,[particle_type],cell,True)
     if i % (num_mc_sweeps/100) == 0:
         acc_ratio = np.float64(accepted_moves)/np.float64(total_moves)
-        print '{0} of {1} MC sweeps done: accepted_ratio = {2}'.format(i,num_mc_sweeps,acc_ratio)
+        print '{0:6d} of {1:6d} MC sweeps done: accepted_ratio = {2:7.5f}'.format(i+1,num_mc_sweeps,acc_ratio)
---
+++ LJ-MC-sampling/ParticleTools.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+
+def randomizePostions(postions,cell,random_seed=None):
+    if random_seed is not None: np.random.seed(random_seed)
+    for i in xrange(postions.shape[0]):
+        postions[i][0] = np.random.uniform(0,cell[0])
+        postions[i][1] = np.random.uniform(0,cell[1])
+        postions[i][2] = np.random.uniform(0,cell[2])
+#-------------------------------
+
+def randomDisplacement(postions,max_displacement,particle_index,random_seed=None):
+    indexes = [particle_index]
+    if random_seed is not None: np.random.seed(random_seed)
+    for k in indexes:
+        postions[k][0] += max_displacement * np.random.uniform(-1,1)
+        postions[k][1] += max_displacement * np.random.uniform(-1,1)
+        postions[k][2] += max_displacement * np.random.uniform(-1,1)
+    #-------------------------------
+
+def writePostionsToFile(filename,postions,particle_names,cell=None,append=True):
+    num_particles = postions.shape[0]
+    if len(particle_names)==1: particle_names = particle_names*num_particles
+    if append:
+        f = open(filename,'a')
+    else:
+        f = open(filename,'w')
+
+    f.write("  {0}\n\n".format(num_particles))
+    for i in xrange(num_particles):
+        a = particle_names[i]
+        p = postions[i]
+        if cell is not None: p = p - np.floor(p/cell) * cell
+        out_str = "  {0}   {1:20.9f}  {2:20.9f}  {3:20.9f}\n".format(a,p[0],p[1],p[2])
+        f.write(out_str)
+    f.close()
+    #-------------------------------
+
+def getSquaredDistance(pos_i,pos_j,cell=None):
+    vec = pos_i - pos_j
+    if cell is not None: vec = vec - np.rint(vec/cell) * cell
+    return np.sum(vec**2)
+#-------------------------------
+
+def getAllSquaredDistances(postions,r2_cutoff=float('inf'),cell=None):
+    distances = []
+    num_particles = postions.shape[0]
+    for i in xrange(num_particles):
+        for j in xrange(i+1,num_particles):
+            r2 = getSquaredDistance(postions[i],postions[j],cell)
+            if r2<r2_cutoff: distances.append(r2)
+    return np.array(distances)
+#-------------------------------
+
+def getAllSquaredDistancesForOneParticle(postions,index,r2_cutoff=float('inf'),cell=None):
+    distances = []
+    num_particles = postions.shape[0]
+    for i in xrange(num_particles):
+        if i==index: continue
+        r2 = getSquaredDistance(postions[index],postions[i],cell)
+        if r2<r2_cutoff: distances.append(r2)
+    return np.array(distances)
+#-------------------------------
+
+
+#-------------------------------
