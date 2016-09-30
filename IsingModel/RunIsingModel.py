d93459a0c0d24dd3b0f41a103c84459efd9bbb90
Omar Valsson <omar.valsson@gmail.com>
2016-09-30 07:36:06 +0200

    Ising

--- IsingModel/RunIsingModel.py
+++ IsingModel/RunIsingModel.py
@@ -5,9 +5,10 @@ import matplotlib.pyplot as plt
 from DataTools import *
 
 NumSweeps = 10000
+T=1.0
 
-plotSpinStateEvery = 10
-m1 = IsingModel(FigNum=1, L=11, Temperature=3.0, Dimension=2,InitialState='random')
+plotSpinStateEvery = 1000
+m1 = IsingModel(FigNum=1, L=41, Temperature=T, Dimension=2,InitialState='random')
 # m1.randomizeSpins()
 
 energyPerSpin = []
@@ -25,7 +26,8 @@ for i in range(NumSweeps):
     absMagnetizationPerSpin.append( m1.getAbsoluteMagnetizationPerSpin() )
     if (i+1) % plotSpinStateEvery == 0:
         m1.updateSpinFig()
-
+    if (i+1) % (NumSweeps/100) == 0:
+        print '  {0:8d} of {1:8d} MC sweeps finished'.format(i+1,NumSweeps)
 energyPerSpin = np.array(energyPerSpin)
 magnetizationPerSpin = np.array(magnetizationPerSpin)
 absMagnetizationPerSpin = np.array(absMagnetizationPerSpin)
@@ -33,18 +35,18 @@ absMagnetizationPerSpin = np.array(absMagnetizationPerSpin)
 data = [energyPerSpin,magnetizationPerSpin,absMagnetizationPerSpin]
 dataFields = ['energyPerSpin','magnetizationPerSpin','absMagnetizationPerSpin']
 
-# writeDataToFile('Ising.T30.data', data, fieldNames=dataFields, addTimeField=True)
+writeDataToFile('Ising.T-'+str(T)+'.data', data, fieldNames=dataFields, addTimeField=True)
 
-plt.figure(2)
-plt.plot(energyPerSpin)
-plt.draw()
-plt.ylim(-2.1,0.6)
-plt.figure(3)
-plt.plot(magnetizationPerSpin)
-plt.ylim(-1.1,1.1)
-plt.draw()
-plt.figure(4)
-plt.plot(absMagnetizationPerSpin)
-plt.ylim(-0.1,1.1)
-plt.draw()
-time.sleep(100.0)
+# plt.figure(2)
+# plt.plot(energyPerSpin)
+# plt.draw()
+# plt.ylim(-2.1,0.6)
+# plt.figure(3)
+# plt.plot(magnetizationPerSpin)
+# plt.ylim(-1.1,1.1)
+# plt.draw()
+# plt.figure(4)
+# plt.plot(absMagnetizationPerSpin)
+# plt.ylim(-0.1,1.1)
+# plt.draw()
+# time.sleep(100.0)
