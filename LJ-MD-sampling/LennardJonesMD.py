75c2206364335d58ff86965cf9536995cf0d7e4a
Omar Valsson <omar.valsson@gmail.com>
2016-12-15 14:43:17 +0100

    Changes

--- LJ-MD-sampling/LennardJonesMD.py
+++ LJ-MD-sampling/LennardJonesMD.py
@@ -7,32 +7,32 @@ epsilon = 1.0
 m = 1.0
 kB = 1.0
 
-r_cutoff = 2.5
+r_cutoff = float('inf')
 r2_cutoff = r_cutoff**2
 ener_shift = 4.0*epsilon*(1.0/r2_cutoff**6-1.0/r2_cutoff**3)
 
-def getPotentialAndForces(p,cell=None):
+def getPotentialAndForces(p,cell):
     nump = p.shape[0]
-    forces = np.zeros([nump,3])
+    forces = np.zeros(p.shape)
     pot = 0.0
     for i in range(nump):
         for j in range(i+1,nump):
             vec = p[i]-p[j]
             if cell is not None: vec = vec - np.rint(vec/cell) * cell
-            r2 = vec[0]**2 + vec[1]**2 + vec[2]**2
+            r2 = np.sum(vec**2)
             if r2 <= r2_cutoff:
                 inv_r2 = sigma**2/r2
                 pot += 4.0*epsilon*(inv_r2**6-inv_r2**3)-ener_shift
-                ff = -24.0*inv_r2*(2.0*inv_r2**6-inv_r2**3)
-                forces[i] += p[i]*ff
-                forces[j] += p[j]*ff
+                ff = 24.0*inv_r2**4*(2.0*inv_r2**3-1.0)
+                forces[i] += vec*ff
+                forces[j] -= vec*ff
     return (pot,forces)
 #-------------------------------
 
 def getKineticEnergy(v):
     kin = 0.0
     for i in range(v.shape[0]):
-        kin += m * np.sum(v[k]**2)
+        kin += m * np.sum(v[i]**2)
     return kin
 #-------------------------------
 
@@ -43,20 +43,22 @@ def initializeVeloctites(v,T,seed):
     v = np.random.normal(mean,stddev,v.shape)
 #-------------------------------
 
-T = 0.4
+T = 0.1
 seed = 15234
 #
-input_grofile = 'LJ128-Solid.gro'
-fn_traj_gro = "traj.gro"
+dim = 2
+input_geo = 'LJ7-2D-initial.xyz'
+fn_traj = "traj.xyz"
 #
 dt = 0.005
-num_steps = 1000
+num_steps = 10000
 
-(postions_in, cell_in) = readPostionsFromFileGro(input_grofile)
+(postions_in, cell_in) = readPostionsFromFileXYZ(input_geo,dim)
 nump = postions_in.shape[0]
+dof = dim*nump
 p = postions_in
 cell = cell_in
-v = np.zeros([nump,3])
+v = np.zeros(p.shape)
 initializeVeloctites(v,T,seed)
 header_gro = "LJ" + str(nump) + ": time={0}"
 
@@ -70,9 +72,8 @@ times = np.arange(num_steps+1)*dt
 potential_energy[0] = pot
 kinetic_energy[0] = getKineticEnergy(v)
 
-print v
-print p
-print F
+writePostionsToFileXYZ(fn_traj,p,['Ar'],None,False)
+writePostionsToFileXYZ('forces.data',F,['Ar'],None,False)
 
 
 for i in range(num_steps):
@@ -82,14 +83,18 @@ for i in range(num_steps):
     kinetic_energy[i+1] = getKineticEnergy(v)
     potential_energy[i+1] = pot_new
     F = Fnew
-total_energy = potential_energy + kinetic_energy
+    writePostionsToFileXYZ(fn_traj,p,['Ar'])
 
+total_energy = potential_energy + kinetic_energy
+temperature = kinetic_energy/(dof*kB)
 
 plt.figure(3)
 plt.plot(times,potential_energy)
 plt.figure(4)
 plt.plot(times,kinetic_energy)
 plt.figure(5)
-plt.ylim(0, np.max(total_energy)+1.0)
+#plt.ylim(np.min(total_energy)*1.1, np.max(total_energy)*1.1)
 plt.plot(times,total_energy)
+plt.figure(6)
+plt.plot(times,temperature)
 plt.show()
--- LJ-MD-sampling/TrajectoryIO.py
+++ LJ-MD-sampling/TrajectoryIO.py
@@ -3,6 +3,7 @@ import numpy as np
 
 def writePostionsToFileXYZ(filename,postions,particle_names,cell=None,append=True):
     num_particles = postions.shape[0]
+    dim = postions.shape[1]
     if len(particle_names)==1: particle_names = particle_names*num_particles
     if append:
         f = open(filename,'a')
@@ -12,7 +13,8 @@ def writePostionsToFileXYZ(filename,postions,particle_names,cell=None,append=Tru
     f.write("  {0}\n\n".format(num_particles))
     for i in range(num_particles):
         a = particle_names[i]
-        p = postions[i]
+        p = np.zeros(3)
+        p[:dim] = postions[i]
         if cell is not None: p = p - np.floor(p/cell) * cell
         out_str = "  {0}   {1:20.9f}  {2:20.9f}  {3:20.9f}\n".format(a,p[0],p[1],p[2])
         f.write(out_str)
@@ -21,6 +23,7 @@ def writePostionsToFileXYZ(filename,postions,particle_names,cell=None,append=Tru
 
 def writePostionsToFileGro(filename,postions,particle_names,header,cell=None,append=True):
     num_particles = postions.shape[0]
+    dim = postions.shape[1]
     if len(particle_names)==1: particle_names = particle_names*num_particles
     if append:
         f = open(filename,'a')
@@ -31,7 +34,8 @@ def writePostionsToFileGro(filename,postions,particle_names,header,cell=None,app
     for i in range(num_particles):
         rstr = "SOL"
         a = particle_names[i]
-        p = postions[i]
+        p = np.zeros(3)
+        p[:dim] = postions[i]
         if cell is not None: p = p - np.floor(p/cell) * cell
         out_str = "{0:5d}{1:5s}{2:5s}{3:5d}{4:8.3f}{5:8.3f}{6:8.3f}\n".format(i,rstr,a,i,p[0],p[1],p[2])
         f.write(out_str)
@@ -41,25 +45,39 @@ def writePostionsToFileGro(filename,postions,particle_names,header,cell=None,app
     f.close()
     #-------------------------------
 
-def readPostionsFromFileGro(filename):
+def readPostionsFromFileGro(filename,dim=3):
     f = open(filename,'r')
     rawdata = f.readlines()
     f.close()
     #
     num_particles = int(rawdata[1])
     #
-    cell_str = rawdata[-1]
+    cell_str = rawdata[-1].split()
     cell = []
-    cell.append(float(cell_str[0:10]))
-    cell.append(float(cell_str[10:20]))
-    cell.append(float(cell_str[20:30]))
+    cell.append(float(cell_str[0]))
+    cell.append(float(cell_str[1]))
+    cell.append(float(cell_str[2]))
     cell = np.array(cell)
-    postions = np.zeros([num_particles,3])
+    postions = np.zeros([num_particles,dim])
     #
     for i in range(num_particles):
-        l = rawdata[i+2]
-        postions[i][0] = float(l[20:28])
-        postions[i][1] = float(l[28:36])
-        postions[i][2] = float(l[36:44])
+        l = rawdata[i+2].split()
+        for k in range(dim): postions[i][k] = float(l[3+k])
+    return postions,cell
+#-------------------------------
+
+def readPostionsFromFileXYZ(filename,dim=3):
+    f = open(filename,'r')
+    rawdata = f.readlines()
+    f.close()
+    #
+    cell = None
+    #
+    num_particles = int(rawdata[0])
+    postions = np.zeros([num_particles,dim])
+    #
+    for i in range(num_particles):
+        l = rawdata[i+2].split()
+        for k in range(dim): postions[i][k] = float(l[1+k])
     return postions,cell
 #-------------------------------
